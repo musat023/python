8aad99bd8bf6cd626cd342387a3c50915e14304d
musat023 <145437556+musat023@users.noreply.github.com>
2024-02-27 11:44:18 +0100

    Update School.py

--- School.py
+++ School.py
@@ -158,7 +158,16 @@
 #arr[4], arr[0] = arr[0], arr[4]
 #print(arr)
 
+#def selection_sort(arr):
+#    for i in range(len(arr)-1):
+#        min_index = i
+#        for j in range(i+1, len(arr)):
+#            if arr[j] < arr[min_index]:
+#                min_index = j
+#        arr[i], arr[min_index] = arr[min_index], arr[i]
+#    return arr
 
+#print(selection_sort([5, 7, 3, 2, 6]))
 
 
 
