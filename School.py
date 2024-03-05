6b7afd5b062632c52d90e3516a4234564a833053
musat023 <145437556+musat023@users.noreply.github.com>
2024-03-05 11:40:40 +0100

    Update School.py

--- School.py
+++ School.py
@@ -169,7 +169,42 @@
 
 #print(selection_sort([5, 7, 3, 2, 6]))
 
+##########################################################################################################################################################
 
+#word = "valorant"
+#g_letters = []
+#lives = 8
+
+
+#while True:
+#    display = ""
+#    for letter in word:
+#        if letter in g_letters:
+#            display += letter
+#        else:
+#            display += " "
+#    print("\nslovo:", display)
+#    print("životy:", lives)
+    
+#    guess = input("hádej písmeno: ")
+    
+#    if guess in g_letters:
+#        print("tohle písmeno jsi už uhádl")
+#        continue
+    
+#    g_letters.append(guess)
+    
+#    if guess not in word:
+#        print("špatně")
+#        lives -= 1
+#        if lives == 0:
+#            print("konec hry. Slovo bylo:", word)
+#            break
+#    else:
+#        print("správně")
+#        if all(letter in g_letters for letter in word):
+#            print("vyhral jsi, slovo je:", word)
+#            break
 
 
 
