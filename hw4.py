c7b8060d2a91c1f56c260d98b970da4f842a48c6
musat023 <145437556+musat023@users.noreply.github.com>
2024-05-28 11:46:52 +0200

    Update hw4.py

--- hw4.py
+++ hw4.py
@@ -1,7 +1,27 @@
-lamps = list(input())
+def zero(func=None): return 0 if func is None else func(0)
+def one(func=None): return 1 if func is None else func(1)
+def two(func=None): return 2 if func is None else func(2)
+def three(func=None): return 3 if func is None else func(3)
+def four(func=None): return 4 if func is None else func(4)
+def five(func=None): return 5 if func is None else func(5)
+def six(func=None): return 6 if func is None else func(6)
+def seven(func=None): return 7 if func is None else func(7)
+def eight(func=None): return 8 if func is None else func(8)
+def nine(func=None): return 9 if func is None else func(9)
 
-for i in range(1, len(lamps)-1):
-    if lamps[i-1] == 'O' and lamps[i] == 'O' and lamps[i+1] == 'O':
-        lamps[i] = '-'
 
-print(''.join(lamps))
+def plus(x):
+    return lambda y: y + x 
+
+def minus(x):
+    return lambda y: y - x
+
+def divided_by(x):
+    return lambda y: y // x
+
+def times(x):
+    return lambda y: y * x
+
+    
+
+print(two(times(nine())))
